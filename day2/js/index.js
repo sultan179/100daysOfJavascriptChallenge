f86c846c5621b9af2e91412203f4683e1dd0aa18
Sultan Arafat <sultanarafat10@gmail.com>
2022-03-11 19:44:18 -0700

    day2-Change bf color

---
+++ day2/js/index.js
@@ -0,0 +1,29 @@
+//change the background RGB color randomly when the button is clicked
+const bg = document.querySelector("body");
+const btn = document.getElementById("btn");
+console.log("hey");
+
+btn.addEventListener("click", ChangeColor);
+
+function ChangeColor() {
+  console.log("hello");
+  let RGB = "";
+  setInterval(function(){
+    for (let i = 0; i < 100; i++) {
+    RGB =
+      "rgb" +
+      "(" +
+      Math.round(Math.random() * 255).toString() +
+      "," +
+      Math.round(Math.random() * 255).toString() +
+      "," +
+      Math.round(Math.random() * 255).toString() +
+      ")";
+    // RGB = RGB.trim();
+    console.log(typeof RGB);
+    bg.style.backgroundColor = RGB;
+
+  }},1000)
+  
+  
+}
